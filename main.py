3068cd52766e7caf4a6c6c5ac42f73687c36c810
Ishan Shah <ishan0102@utexas.edu>
2023-03-29 14:03:20 -0700

    Use Sieve reference to share models globally

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from typing import Dict
 
 import sieve
 
-from captioner import ImageCaptioner
-from splitter import video_splitter
-
 
 @sieve.function(
     name="sort-captions",
@@ -13,12 +10,33 @@ from splitter import video_splitter
 )
 def sort_captions(captions: Dict) -> str:
     sorted_captions = sorted(captions, key=lambda x: x["frame_number"])
-    return "\n".join([x["caption"] for x in sorted_captions])
+    for caption in sorted_captions:
+        yield caption["caption"]
+
+
+@sieve.function(
+    name="concatenate-features",
+    iterator_input=True,
+    persist_output=True,
+)
+def concatenate_features(sorted_captions, sort_outputs) -> Dict:
+    return {
+        "captions": [c for c in sorted_captions],
+        "sort_outputs": [o for o in sort_outputs],
+    }
 
 
 @sieve.workflow(name="video_qa")
-def video_qa(video: sieve.Video, question: str):
-    images = video_splitter(video, question)
-    captions = ImageCaptioner()(images)
+def video_qa(video: sieve.Video, question: str) -> Dict:
+    images = sieve.reference("ishan0102-utexas-edu/video-splitter-with-metadata")(video, question)
+
+    # Image captioning
+    captions = sieve.reference("ishan0102-utexas-edu/image-captioner")(images)
     sorted_captions = sort_captions(captions)
-    return sorted_captions
+
+    # YOLO/SORT
+    yolo_outputs = sieve.reference("ishan0102-utexas-edu/yolo")(images)
+    sort_outputs = sieve.reference("ishan0102-utexas-edu/sort")(yolo_outputs)
+
+    concatenated_features = concatenate_features(sorted_captions, sort_outputs)
+    return concatenated_features
